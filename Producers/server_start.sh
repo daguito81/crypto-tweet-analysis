69cb799f245f87b5c0ceeebbfce73a2c7c1a07e8
daguito81 <dagoromer85@gmail.com>
2019-04-11 10:07:03 +0200

    uncommented kafka starters on script and updated gitignore

--- Producers/server_start.sh
+++ Producers/server_start.sh
@@ -3,9 +3,9 @@
 sudo /opt/zookeeper/bin/zkServer.sh start /opt/zookeeper/conf/zookeeper.properties
 echo Zookeper started...
 
-#sudo /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server1.properties &
-#sudo /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server2.properties &
-#sudo /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server3.properties &
+sudo /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server1.properties &
+sudo /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server2.properties &
+sudo /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server3.properties &
 
 echo Kafka brokers online...
 
