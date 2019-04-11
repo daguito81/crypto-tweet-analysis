8c6f0a68f268297a6c74e7dd2b0e8d92057efaf4
daguito81 <dagoromer85@gmail.com>
2019-04-11 09:59:43 +0200

    fixed issue #5 with start/stop scripts

--- Producers/server_stop.sh
+++ Producers/server_stop.sh
@@ -6,6 +6,6 @@ echo Killed Kafka brokers...
 
 sleep 10s
 
-sudo /opt/zookeeper/bin/zkServer.sh stop
+sudo /opt/zookeeper/bin/zkServer.sh stop /opt/zookeeper/conf/zookeeper.properties
 
 echo Killed Zookeeper server...
